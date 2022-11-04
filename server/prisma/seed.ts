fb41a1f2a414cf22f60a3419fbfc45a3ea0fe894
DioneDw <dionewilly@hotmail.com>
2022-11-03 23:10:09 -0300

    seed-db-create

---
+++ server/prisma/seed.ts
@@ -0,0 +1,63 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() { 
+  const user = await prisma.user.create({
+data: {
+  name: 'Dione ',
+  email:'didi-ficticio@gmail.com',
+  avatarUrl: 'https://github.com/dionedw.png'
+}
+  })
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Exemplo do bolao',
+      code: 'bolaoteste',
+      ownerId: user.id,
+
+    participants: {
+      create:{ 
+      userId:user.id,
+        }
+    }
+    }
+   })
+
+   await prisma.game.create({
+    data: {
+      date: '2022-11-03T12:00:00.274Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    }
+   })
+
+   await prisma.game.create({
+    data: {
+      date: '2022-11-03T13:00:00.274Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+     guesses: {create:{
+      firstTeamPoints: 2,
+      secondTeamPoits: 1,
+      
+       participant: {connect: {
+         userId_poolId: {
+          userId: user.id,
+          poolId: pool.id
+         }
+       }}
+     }}
+    }
+   })
+
+  // const participant = await prisma.participant.create({
+  //   data: {
+  //     poolId: pool.id,
+  //     userId: user.id
+  //   }
+  // })
+}
+
+main()
