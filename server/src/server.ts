b20dd9e7e0d32f3a1fcc4e44807c85f97332a6c8
DioneDw <dionewilly@hotmail.com>
2022-11-04 23:13:05 -0300

    routes-complete

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import Fastify from "fastify";
-import { PrismaClient} from '@Prisma/client';
+import { PrismaClient} from '@prisma/client';
+import { z } from 'zod';
+import ShortUniqueID from 'short-unique-id';
+
 
 import cors from '@fastify/cors';
 
@@ -26,7 +29,48 @@ fastify.get('/pools/count', async ()=> {
   return {count}
 })
 
-await fastify.listen({ port:3333, host:'0.0.0.0'})
+// Criando um bolão 
+fastify.post('/pools', async (request, reply)=> {
+ const createdPoolBody = z.object({
+  title: z.string(),
+ })
+ const {title} = createdPoolBody.parse(request.body)
+
+ const generate = new ShortUniqueID({length:6})
+ const code= String(generate()).toUpperCase() 
+
+ await prisma.pool.create({
+  data: {
+    title,
+    code
+  }
+ })
+
+  reply.status(201).send(code)
+
+ return { code }
+ })
+
+// Contagens de usuário - bolão
+fastify.get('/users/count', async ()=> {
+  const count= await prisma.user.count()
+ 
+   return {count}
+ })
+
+// Contagens de palpites - bolão
+fastify.get('/guesses/count', async ()=> {
+  const count= await prisma.guess.count()
+ 
+   return {count}
+ })
+
+
+
+await fastify.listen({ port:3333})
+
+// , host:'0.0.0.0'
 
 }
 bootstrap()
+
